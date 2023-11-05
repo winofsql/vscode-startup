5f5b4938347154da41687ce9014b5d324c771f96
sworc <lightbox@nifty.com>
2023-11-05 10:09:26 +0900

    Add files via upload

---
+++ code-install-ext.bat
@@ -0,0 +1,19 @@
+REM vscode > F1 > Configure Display Language
+echo vscode > F1 > Configure Display Language
+
+cmd /c code --install-extension ms-ceintl.vscode-language-pack-ja  --force
+cmd /c code --install-extension adrianwilczynski.terminal-commands --force
+cmd /c code --install-extension formulahendry.code-runner --force
+cmd /c code --install-extension ms-dotnettools.csharp --force
+cmd /c code --install-extension fudge.auto-using --force
+cmd /c code --install-extension phu1237.vs-browser --force
+cmd /c code --install-extension tomoki1207.pdf --force
+cmd /c code --install-extension arcanis.vscode-zipfs  --force
+cmd /c code --install-extension janisdd.vscode-edit-csv --force
+cmd /c code --install-extension grapecity.gc-excelviewer --force
+cmd /c code --install-extension vscjava.vscode-java-pack --force
+cmd /c code --install-extension ms-vscode.hexeditor --force
+cmd /c code --install-extension cweijan.vscode-mysql-client2 --force
+cmd /c code --install-extension pkief.material-icon-theme --force
+cmd /c code --install-extension adpyke.vscode-sql-formatter --force
+cmd /c code --install-extension serpen.vbsvscode --force
